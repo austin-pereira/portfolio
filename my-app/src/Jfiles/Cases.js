ccbc8f1efaa5d470b61c6473049e4f15be8f75e7
Austin <pereira.austin6602@berkeley.edu>
2024-08-06 17:42:10 +0530

    changes done

--- my-app/src/Jfiles/Cases.js
+++ my-app/src/Jfiles/Cases.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../Cfiles/Cases.css'; // Import your CSS file
 
 const Cases = () => {
   return (
-    <div>Cases</div>
-  )
-}
+    <div className="cases-container">
+      <h2>Case Studies</h2>
+      <ul className="case-study-list"> {/* Use an unordered list for bullet points */}
+        <li>
+          <a href="https://medium.com/@austinpereira6602/case-study-1-8ec2fe2e9f4a" target="_blank" rel="noopener noreferrer">
+            Roomba Case Study
+          </a>
+        </li>
+        <li>
+          <a href="https://medium.com/@austinpereira6602/chatgpt-9628d5368afd" target="_blank" rel="noopener noreferrer">
+            ChatGPT case Study
+          </a>
+        </li>
+        {/* Add more case study links as needed */}
+      </ul>
+    </div>
+  );
+};
 
-export default Cases
+export default Cases;
